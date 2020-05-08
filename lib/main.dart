fd8561bce564d30421081ccf21a5be3d9c5fa14a
Ryota Okuno <>
2020-05-08 18:45:47 +0900

    タイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('パスワード管理'),
       ),
       body: Center(
         child: Column(
